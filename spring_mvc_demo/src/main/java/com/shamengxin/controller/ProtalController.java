b16d636970b80b9da9c3fc3949b4a12bf63862df
shamengxin <1921028746@qq.com>
2023-03-21 23:26:15 +0800

    3.21

---
+++ spring_mvc_demo/src/main/java/com/shamengxin/controller/ProtalController.java
@@ -0,0 +1,14 @@
+package com.shamengxin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProtalController {
+
+    @RequestMapping("/")
+    public String protal(){
+        return "index";
+    }
+
+}
